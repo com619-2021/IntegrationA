f5f64acb0b91c98c092b74ade8fd3e79402609a4
tom-lange-dev <tom-lange@outlook.com>
2021-11-07 16:02:09 +0000

    added stevedore A

---
+++ experimental/StevedoreAppA-main/web/src/test/java/uk/ca/solent/devops/stevedore/UnitTest.java
@@ -0,0 +1,12 @@
+package uk.ca.solent.devops.stevedore;
+
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+
+    @Test
+    public void test(){
+        System.out.println("Hallo");
+    }
+
+}
